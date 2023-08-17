989602643e594f9f688032b7e7c366afc6e82ee1
radian628 <70352880+radian628@users.noreply.github.com>
2023-08-16 21:35:45 -0700

    added hash nav

---
+++ index.js
@@ -0,0 +1,29 @@
+const headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+
+for (const header of headers) {
+  const innerHTML = header.innerHTML;
+  const a = document.createElement("a");
+  a.href = `#${header.id}`;
+  a.innerHTML = innerHTML;
+  const children = Array.from(header.children);
+  header.innerHTML = "";
+  header.appendChild(a);
+}
+
+let lastSelectedSection;
+
+const hashchange = () => {
+  const hash = window.location.hash;
+
+  if (hash) {
+    const selectedSection = document.querySelector(hash)?.parentElement;
+
+    lastSelectedSection?.classList.toggle("selected-section", false);
+    lastSelectedSection = selectedSection;
+    selectedSection.classList.toggle("selected-section", true);
+  }
+};
+
+window.addEventListener("hashchange", hashchange);
+
+hashchange();
